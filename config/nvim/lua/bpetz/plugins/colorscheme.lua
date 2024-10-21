f30a69c5cf9e19ed0f289c46c52ddea4fece6610
Bryan Petzinger <bp2070@gmail.com>
2024-10-20 20:10:41 -0400

    switch colorscheme to catppuccin

--- config/nvim/lua/bpetz/plugins/colorscheme.lua
+++ config/nvim/lua/bpetz/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
