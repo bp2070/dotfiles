17edc853567d9f3b507f28cfb201245d0af0eb2c
bryan.petzinger.civ@army.mil <bryan.petzinger.civ@army.mil>
2025-04-21 17:21:18 -0400

    add keymap: <CR> in normal mode adds blank line

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
+
 vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+
+-- Add empty lines before and after cursor line
+vim.keymap.set("n", "<CR>", "o<Esc>")
