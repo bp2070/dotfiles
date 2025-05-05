0492613e5b8f3124ce5a5b2cf07b83854dd520fe
bryan.petzinger.civ@army.mil <bryan.petzinger.civ@army.mil>
2025-05-05 09:58:54 -0400

    nvim pick show frecency scores

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        debug = {
+          scores = true,
+        },
+        matcher = {
+          frecency = true,
+        },
+      },
+    },
+  },
+}
