cc7ce7c37b42d23e88971e542adb7848a7e87b77
Petzinger, Bryan <bryan.petzinger.civ@army.mil>
2026-01-23 16:44:26 -0500

    change snacks picker and explorer to show hidden files by default

--- .chezmoitemplates/nvim/lua/plugins/snacks_picker.lua
+++ .chezmoitemplates/nvim/lua/plugins/snacks_picker.lua
@@ -1,6 +1,14 @@
 return {
   {
     "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        hidden = true,
+      },
+      picker = {
+        hidden = true,
+      },
+    },
     keys = {
       { "<leader>/", LazyVim.pick("grep", { root = false }), desc = "Grep (cwd)" },
       { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
